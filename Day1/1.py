e54e826c3006e7183ae03cbd4194d8cc11092c47
Jagddish <jagddish13@gmail.com>
2021-10-12 12:05:26 +0530

    Day 1

---
+++ Day1/1.py
@@ -0,0 +1,20 @@
+#  introduced to  print()
+#  strings
+# doubl quotes shows the begining and ending of a string
+# \n for new line
+# tab or spaces(4) to indent
+# print("Hello World\nHello world")
+# new_input = input("ask what you want to input")
+
+name = input("What is your name?")
+print(name)
+
+name = 'Jack'
+print(name)
+
+name = 'Angela'
+print(name)
+
+user_name = input("what is your name")
+length = len(user_name)
+print(length)
---
+++ Day1/project1.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name generator.")
+
+city = input("What's name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print(f"Your band name could be {city} {pet}")
