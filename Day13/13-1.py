e6f46ad253b983b0c2c12b16d591ad9438f729f3
Jagddish <jagddish13@gmail.com>
2021-10-12 12:08:01 +0530

    Day 13

---
+++ Day13/13-1.py
@@ -0,0 +1,9 @@
+#  Debugging
+
+number = int(input("Which number do you want to check? "))
+
+# Here number divided and remainder should be equal to 0 not assign to 0.
+if number % 2 = 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number")
