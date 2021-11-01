c97465483ec3f077df0a412ae027be2f4680c8f0
Jagddish <jagddish13@gmail.com>
2021-11-02 00:27:55 +0530

    Day 22

--- Day22/main.py
+++ Day22/main.py
@@ -1,13 +1,13 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
-import  time
+import time
 from scoreboard import ScoreBoard
 
 screen = Screen()
-screen.bgcolor('black')
+screen.bgcolor("black")
 screen.setup(width=800, height=600)
-screen.title('Pong')
+screen.title("Pong")
 screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
@@ -17,11 +17,10 @@ ball = Ball()
 scoreboard = ScoreBoard()
 
 
-
 screen.listen()
-screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
@@ -33,8 +32,14 @@ while game_is_on:
     # Detect Collision with walls
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
+
     # Collision with right paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    if (
+        ball.distance(r_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(l_paddle) < 50
+        and ball.xcor() < -320
+    ):
         ball.bounce_x()
 
     # Dectect right paddle misses
